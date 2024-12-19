0db1c7dd44eb54ddf8919e2587e91c032ba527a4
anoborame <zacharymccrth@gmail.com>
2024-12-19 12:03:02 -0500

    Move feature size to model definition.

--- src/resnet18_model_def.py
+++ src/resnet18_model_def.py
@@ -1,15 +1,14 @@
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
 from torchvision.transforms import v2
-import torchvision.models as models
+
+INPUT_FEATURES = 224
+OUTPUT_FEATURES = 4
 
 preprocessing_pipeline = v2.Compose([
     v2.Grayscale(num_output_channels=1),
-    v2.Resize((224, 224)),  # Resize all images to 224x224 (size used for ResNet models)
+    v2.Resize((INPUT_FEATURES, INPUT_FEATURES)),  # Resize all images to 224x224 (size used for ResNet models)
     v2.ToTensor(),  # Convert PIL.Image to torch.Tensor
     v2.Lambda(lambda x: x[:, 10:-10, 10:-10]),
     v2.Lambda(lambda x: x.repeat(3, 1, 1)),
     v2.Normalize(mean=[0], std=[1])
 ])
+
